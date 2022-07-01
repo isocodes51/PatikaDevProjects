797d6ae36ccac57205ab0ab366e7cbaae0549b90
isocodes51 <84030162+isocodes51@users.noreply.github.com>
2022-07-01 13:33:39 +0300

    Create MyMethod.cs
    www.patika.dev

---
+++ Metotlar/MyMethod.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    public class MyMethod
+    {
+     public int Topla(int deger1, int deger2)
+        {
+            return deger1 + deger2;
+        }
+    }
+}
