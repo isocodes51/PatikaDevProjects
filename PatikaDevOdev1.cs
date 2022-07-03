ea173755b8f02c0f31f1de6a5e7cde5a6d50ae98
isocodes51 <84030162+isocodes51@users.noreply.github.com>
2022-07-04 01:09:18 +0300

    Update PatikaDevOdev1.cs
    www.patika.dev C# 101 Ödev 1

--- PatikaDevOdev1.cs
+++ PatikaDevOdev1.cs
@@ -7,20 +7,24 @@ namespace DevPatikaProjeOdev1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozitif bir sayı girin");
-            int n = Convert.ToInt32((Console.ReadLine()));
-            CiftSayiHesapla x = new CiftSayiHesapla();
-            //Console.WriteLine(ciftSayiHesapla.CiftSayilar(n));
-            x.CiftSayilar(n);
+            
+            OdevSorulari x = new OdevSorulari();
+            // x.CiftSayilar(); //1.Soru
+            // x.Sayilar(); //2.Soru
+            //x.Kelimeler(); // 3.soru
+            x.Cumle(); //4.Soru
         }
 
        
     }
 
-    class CiftSayiHesapla
+    class OdevSorulari
     {
-        public void CiftSayilar(int a)
+        public void CiftSayilar()
         {
+            Console.WriteLine("Pozitif bir sayı girin");
+            int a = Convert.ToInt32((Console.ReadLine()));
+
             List<int> nArr = new List<int>();
 
             while (a > 0)
@@ -36,10 +40,70 @@ namespace DevPatikaProjeOdev1
                 if (nArr[i] % 2 == 0)
                     Console.WriteLine("Çift Sayılar" + nArr[i]);
             }
+        }
 
+        public void Sayilar()
+        {
+            Console.WriteLine("1. Sayıyı Gir:");
+            int n = Convert.ToInt32((Console.ReadLine()));
+            Console.WriteLine("2. Sayıyı Gir:");
+            int m = int.Parse((Console.ReadLine()));
+            
+            
+            int[] dizi = new int[n];
 
+            while (n > 0)
+            {
+                Console.WriteLine("Pozitif sayıları Girin:");
+                n--;
+                int n2 = Convert.ToInt32(Console.ReadLine());
+
+                dizi[n] = n2;
+            }
+            for( int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] == m || m % dizi[i]  == 0)
+                    Console.WriteLine("Sayılar: "+dizi[i]);
+
+            }
+        }
+
+        public void Kelimeler()
+        {
+            Console.WriteLine("Kaç Kelime Girilecek:");
+            int n = Convert.ToInt32((Console.ReadLine()));
+            string[] kelimeDizi = new string[n];
+            while (n > 0)
+            {
+                Console.WriteLine("Kelimeleri Girin:");
+                n--;
+                string n2 = Console.ReadLine();
+
+                kelimeDizi[n] = n2;
+            }
+            Array.Reverse(kelimeDizi,n,n);
+            foreach (string s in kelimeDizi)
+            {
+                Console.WriteLine("Reversed: "+s);
+            }
+        }
+
+        public void Cumle()
+        {
+            Console.WriteLine("Cümleyi Girin:");
+            string n = Console.ReadLine();
+            string[] cumleDizi = n.Split(" ");
+            int toplamHarfSayisi = 0;
+            Console.WriteLine("Kelime Sayısı:"+ cumleDizi.Length);
+            foreach(string s in cumleDizi)
+            {
+                toplamHarfSayisi += s.Length;
+
+            }
+            Console.WriteLine(toplamHarfSayisi);
         }
 
     }
+
  }
 
