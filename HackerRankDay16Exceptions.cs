ecc502817a755577dc4775b395bd4c83a01df0cd
isocodes51 <84030162+isocodes51@users.noreply.github.com>
2022-07-05 00:29:28 +0300

    Create HackerRankDay16Exceptions.cs
    www.patika.dev

---
+++ HackerRankDay16Exceptions.cs
@@ -0,0 +1,33 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+        
+        try{
+         int val= Convert.ToInt32(S); 
+          Console.WriteLine(S);   
+        }catch (Exception e){
+            Console.Write("Bad String");
+        }
+       
+        
+        
+    }
+}
