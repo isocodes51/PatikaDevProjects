f754de32ca42b3a8a6f30fed382d29b00e464aca
isocodes51 <84030162+isocodes51@users.noreply.github.com>
2022-07-06 16:40:36 +0300

    Create PatikaDevDateTimeMath.cs
    www.patika.dev

---
+++ PatikaDevDateTimeMath.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace PatikaDevDateTimeMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(Math.Abs(-25));
+            Console.WriteLine(Math.Sqrt(25));
+            Console.WriteLine(Math.Pow(3,4));
+        }
+    }
+}
