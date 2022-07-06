3912b7505ce7956791d215884270f6fae4a3022a
isocodes51 <84030162+isocodes51@users.noreply.github.com>
2022-07-07 01:52:21 +0300

    Create HackerrankMinMaxSum.cs
    www.patika.dev

---
+++ HackerrankMinMaxSum.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    { long sumBig=0;
+      long sumSmall=0;
+      long sum=0;
+      for(int i=0;i<arr.Count;i++){
+        sum += arr[i];  
+      } 
+      sumBig=sum-arr.Min();
+      sumSmall=sum-arr.Max();
+      Console.Write(sumSmall+" "+sumBig);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
